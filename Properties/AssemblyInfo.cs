fb42e8c99b9bc92a1f17a6baf22048605ea9e780
Thekillergreece <sakislkg@gmail.com>
2025-09-28 19:59:29 +0300

    v3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("AmbientLights")]
-[assembly: AssemblyCopyright("Created by Xpazeman")]
-[assembly: AssemblyVersion("2.8.1")]
-[assembly: AssemblyFileVersion("2.8.1")]
+[assembly: AssemblyCopyright("Created by Xpazeman, maintained by Thekillergreece")]
+[assembly: AssemblyVersion("3")]
+[assembly: AssemblyFileVersion("3")]
 
-[assembly: MelonInfo(typeof(AmbientLights.AmbientLights), "AmbientLights", "2.8.1", "Xpazeman", null)]
+[assembly: MelonInfo(typeof(AmbientLights.AmbientLights), "AmbientLights", "3", "Thekillergreece", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
